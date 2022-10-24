7ba93a6eae182170ccbf7a6aed5cca33b5b73e7a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-24 14:04:44 +0500

    بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ

---
+++ helloworld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract MyContract {
+    function helloWorld() public pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
+
+//contract HelloWorld{
+//	string public myString = "Assalam-O-Alaikum World!";
+//}
